87e367ea5ed974f9c9eb97a0aae411d4aba1b06e
Jithin Madhusudanan Sreekala <jithin.madhusudanan.sreekala@cern.ch>
2019-11-05 08:27:18 -0600

    removed unwanted file

--- study/runAnalyzer_study.sh
+++ study/runAnalyzer_study.sh
@@ -1,7 +1,7 @@
 
 ./rootcom Analyzer_mutau_study analyze_mutau_study
 start=`date +%s`
-./analyze_mutau_study /hdfs/store/user/jmadhusu/_MC2018_Autumn18_monoHiggs_05Nov/GluGluHToTauTau_M125_13TeV_powheg_pythia8/crab_job_GluGluHToTauTau/191105_035032/0000/ study_mutau_110k.root 1000 100 2018_test
+./analyze_mutau_study /hdfs/store/user/jmadhusu/_MC2018_Autumn18_monoHiggs_05Nov/GluGluHToTauTau_M125_13TeV_powheg_pythia8/crab_job_GluGluHToTauTau/191105_035032/0000/ study_mutau_110k.root 110000 100 2018_test
 #./analyze_mutau_study /hdfs/store/user/jmadhusu/_MC2018_Autumn18_monoHiggs_05Nov/GluGluHToTauTau_M125_13TeV_powheg_pythia8/crab_job_GluGluHToTauTau/191105_035032/0001/ study_mt_2.root -1 1000 2018_test
 end=`date +%s`
 runtime=$((end-start))
