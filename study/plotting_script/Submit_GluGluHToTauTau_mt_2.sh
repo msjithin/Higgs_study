6eb8d52ac41a41d9d890614f8acf8c61ab202fd3
Jithin Madhusudanan Sreekala <jithin.madhusudanan.sreekala@cern.ch>
2020-02-18 09:03:30 -0600

    debugging reco

---
+++ study/plotting_script/Submit_GluGluHToTauTau_mt_2.sh
@@ -0,0 +1,7 @@
+
+
+start=`date +%s`
+./analyze_mutau_study /hdfs/store/user/jmadhusu/MC2018_Autumn18_monoHiggs_23Jan/GluGluHToTauTau_M125_13TeV_powheg_pythia8/crab_job_GluGluHToTauTau/200122_102019/0001/ study_mt_2.root -1 1000 2018_test
+end=`date +%s`
+runtime=$((end-start))
+echo "Runtime = $runtime"
---
+++ study/runAnalyzer_complete.sh
@@ -0,0 +1,13 @@
+set -e
+
+outFile="study_mutau_complete.root"
+if [ -f "$outFile" ]; then
+    rm $outFile
+fi
+hadd $outFile study_mt_1.root study_mt_2.root
+cd plotting_script/
+cp ../$outFile .
+bash do_ind_plots.sh $outFile
+bash postAnalyzer_mutau.sh $outFile
+cp plot_* ~/public_html/boosted_study/study/ 
+cd ..
